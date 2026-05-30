6fca96d8cea99131a7f631379f116fb88e21572d
gkatsarova <g_katsarova2007@abv.bg>
2026-05-30 17:09:50 +0300

    Add integration tests for AuthRepository

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,6 +105,7 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.mockk)
     testImplementation(libs.kotlinx.coroutines.test)
+    androidTestImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.turbine)
     testImplementation(libs.androidx.core.testing)
 
